76a381b11ad3f080cf9419ff63c86aff7fbdf10e
mcuste <github@muratcanuste.com>
2025-03-02 21:03:12 +0100

    use terminal mux for panes and tabs instead of ghostty

--- nix/home-manager/common/term/ghostty.nix
+++ nix/home-manager/common/term/ghostty.nix
@@ -45,33 +45,15 @@
             "ctrl+shift+c=copy_to_clipboard"
             "ctrl+shift+v=paste_from_clipboard"
 
-            "alt+a>r=reload_config"
-            "alt+a>\\=toggle_window_decorations"
+            "ctrl+shift+e=write_scrollback_file:open"
+            "ctrl+shift+l=clear_screen"
 
-            "alt+a>e=write_scrollback_file:open"
-            "alt+ctrl+l=clear_screen"
+            "ctrl+shift+zero=reset_font_size"
+            "ctrl+shift+minus=decrease_font_size:1"
+            "ctrl+shift+plus=increase_font_size:1"
 
-            "alt+a>q=close_window"
-            "alt+a>c=new_tab"
-            "alt+a>v=new_split:right"
-            "alt+a>s=new_split:down"
-
-            "alt+shift+zero=reset_font_size"
-            "alt+shift+minus=decrease_font_size:1"
-            "alt+shift+plus=increase_font_size:1"
-
-            "alt+shift+up=resize_split:up,10"
-            "alt+shift+down=resize_split:down,10"
-            "alt+shift+right=resize_split:right,10"
-            "alt+shift+left=resize_split:left,10"
-
-            "ctrl+h=goto_split:left"
-            "ctrl+l=goto_split:right"
-            "ctrl+j=goto_split:down"
-            "ctrl+k=goto_split:up"
-
-            "alt+a>h=previous_tab"
-            "alt+a>l=next_tab"
+            "ctrl+shift+r=reload_config"
+            "ctrl+shift+\\=toggle_window_decorations"
           ];
         };
       };
