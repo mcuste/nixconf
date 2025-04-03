6d94c632a11d86437663c3fa45025a31360a7a76
mcuste <github@muratcanuste.com>
2025-04-03 23:38:46 +0200

    remove unnecessary homeserver packages and add stremio

--- nix/home-manager/homeserver-fedora.nix
+++ nix/home-manager/homeserver-fedora.nix
@@ -17,28 +17,13 @@
         systemd = false;
       };
 
-      chat.slack = true;
+      editor = {neovim = true;};
 
-      editor = {
-        neovim = true;
-        obsidian = true;
-        vscode = true;
-      };
-
-      media = {
-        spotify = true;
-      };
+      media = {spotify = true;};
 
       packages = {
         kanata = true;
         protonvpn = true;
-        terraform = true;
-        gcloud = true;
-        kubectl = true;
-        k9s = true;
-        k3d = true;
-        helm = true;
-        argo = true;
       };
     };
   };
